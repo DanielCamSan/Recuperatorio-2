652b22889aea1cad1b14d936a3949c9ca25c0d07
Juanma <juanma.soruco@gmail.com>
2025-12-05 16:24:13 -0400

    feat:relations

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -21,6 +21,14 @@ namespace _3ecexamen.Data
             // 1:N Conference -> Rooms (FK requerida, cascade)
             // N:M con payload: Talk (clave compuesta)
             // (Opcional) Índice único: Room.Name dentro de una Conference
+            modelBuilder.Entity<Room>()
+                .HasOne(r => r.Conference)
+                .WithMany(c => c.Rooms)
+                .HasForeignKey(r => r.ConferenceId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Talk>()
+                .HasKey(t => new { t.SpeakerId, t.RoomId, t.StartTime });
 
         }
     }
