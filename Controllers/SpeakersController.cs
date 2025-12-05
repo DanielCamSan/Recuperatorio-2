5497a590b86a769ac4e45d0102ff0bd763bc6b5c
Juanma <juanma.soruco@gmail.com>
2025-12-05 16:57:32 -0400

    feat:add SpeakersController GetSchedule

--- Controllers/SpeakersController.cs
+++ Controllers/SpeakersController.cs
@@ -42,6 +42,8 @@ namespace _3ecexamen.Controllers
         public async Task<IActionResult> AddTalk([FromBody] CreateTalkDto dto)
         {
             //TODO
+            await _talks.AddTalkAsync(dto);
+            return Ok();
         }
     }
 }
