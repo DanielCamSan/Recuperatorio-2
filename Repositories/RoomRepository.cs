025ed9b001c542d289a8f30e0b34812e8919b7ec
unknown <nicole.herbas@ucb.edu.bo>
2025-12-05 16:18:34 -0400

    feat: adding ExistsAsync to RoomRepository

--- Repositories/RoomRepository.cs
+++ Repositories/RoomRepository.cs
@@ -11,7 +11,7 @@ namespace _3ecexamen.Repositories
 
         public Task<bool> ExistsAsync(int id)
         {
-            //TODO
+            return _ctx.Rooms.AnyAsync(r => r.Id == id);
         }
     }
 }
