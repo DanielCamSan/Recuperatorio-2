ba24e3344b1da52322c65d2e16072bf98c08dcc3
Juanma <juanma.soruco@gmail.com>
2025-12-05 17:17:04 -0400

    feat: add optional in AppDbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -29,7 +29,9 @@ namespace _3ecexamen.Data
 
             modelBuilder.Entity<Talk>()
                 .HasKey(t => new { t.SpeakerId, t.RoomId, t.StartTime });
-
+            modelBuilder.Entity<Room>()
+                .HasIndex(r => new { r.ConferenceId, r.Name })
+                .IsUnique();
         }
     }
 }
