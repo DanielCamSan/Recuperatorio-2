f993b2d9f06688257ec1ac3d7153a8edaaac610c
CalebBav234 <calebmbaving@gmail.com>
2025-12-05 16:37:10 -0400

    Feat: added GetAgendaAsync Method and CreateConferenceAsync method in ConferenceService

--- Services/ConferenceService.cs
+++ Services/ConferenceService.cs
@@ -6,18 +6,49 @@ namespace _3ecexamen.Services
 {
     public class ConferenceService : IConferenceService
     {
-        //TODO
+        private readonly IConferenceRepository _conferenceRepository;
+        public ConferenceService(IConferenceRepository conferenceRepository)=> _conferenceRepository = conferenceRepository;
 
         public async Task<int> CreateConferenceAsync(CreateConferenceDto dto)
         {
-            //TODO
+            var entity = new Conference
+            {
+                Title = dto.Title,
+                City = dto.City,
+                StartDate = DateTime.SpecifyKind(dto.StartDate, DateTimeKind.Utc),
+                EndDate = DateTime.SpecifyKind(dto.EndDate, DateTimeKind.Utc),
+                Rooms = dto.Rooms.Select(r => new Room { Name = r.Name }).ToList()
+            };
+            await _conferenceRepository.AddAsync(entity);
+            await _conferenceRepository.SaveChangesAsync();
+            return entity.Id;
         }
 
         public async Task<ConferenceAgendaDto?> GetAgendaAsync(int id)
         {
-            var conf = await _confs.GetAgendaAsync(id);
+            var conf = await _conferenceRepository.GetAgendaAsync(id);
             if (conf == null) return null;
             //TODO  pista: devuelve usando ConferenceAgendaDto
+            return new ConferenceAgendaDto
+            {
+                Conference = conf.Title,
+                City = conf.City,
+                Rooms = conf.Rooms.OrderBy(r => r.Name)
+                    .Select(r => new RoomScheduleDto
+                    {
+                        Room = r.Name,
+                        Talks = r.Talks.OrderBy(t => t.StartTime)
+                            .Select(t => new TalkDto
+                            {
+                                SpeakerId = t.SpeakerId,
+                                Speaker = t.Speaker.FullName,
+                                RoomId = t.RoomId,
+                                Room = r.Name,
+                                StartTime = t.StartTime,
+                                EndTime = t.EndTime
+                            }).ToList()
+                    }).ToList()
+            };
            
         }
     }
