b5bbd3e0dc1f1532a1ddc7e3fd70b2593361ae70
CalebBav234 <calebmbaving@gmail.com>
2025-12-05 16:41:19 -0400

    Feat: added DI in program.cs

--- Program.cs
+++ Program.cs
@@ -14,7 +14,13 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("Default")));
 
 //TODO 
-
+builder.Services.AddScoped<IConferenceRepository,ConferenceRepository>();
+builder.Services.AddScoped<IRoomRepository,RoomRepository>();
+builder.Services.AddScoped<ISpeakerRepository,SpeakerRepository>();
+builder.Services.AddScoped<ITalkRepository,TalkRepository>();
+builder.Services.AddScoped<IConferenceService,ConferenceService>();
+builder.Services.AddScoped<ISpeakerService,SpeakerService>();
+builder.Services.AddScoped<ITalkService,TalkService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
