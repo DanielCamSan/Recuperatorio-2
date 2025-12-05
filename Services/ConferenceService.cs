47acb6612ae555be4e8e0cb8e23c419498656659
ha55an-priv <davidlopezolivares05@gmail.com>
2025-12-05 16:37:04 -0400

    feat: advances services

--- Services/ConferenceService.cs
+++ Services/ConferenceService.cs
@@ -7,10 +7,17 @@ namespace _3ecexamen.Services
     public class ConferenceService : IConferenceService
     {
         //TODO
+        private readonly IConferenceRepository _confs;
 
         public async Task<int> CreateConferenceAsync(CreateConferenceDto dto)
         {
             //TODO
+            var conf = new Conference
+            {
+                Title = dto.Title,
+                StartDate = dto.StartDate,
+                EndDate = dto.EndDate
+            };
         }
 
         public async Task<ConferenceAgendaDto?> GetAgendaAsync(int id)
