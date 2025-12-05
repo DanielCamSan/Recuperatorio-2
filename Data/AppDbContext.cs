163b93511f42aec08fbaa4b835a9f2e06750f819
unknown <oriana.flores@ucb.edu.bo>
2025-12-05 16:22:58 -0400

    fix: appdbcontext,roomrepository

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -19,9 +19,18 @@ namespace _3ecexamen.Data
             //TODO
 
             // 1:N Conference -> Rooms (FK requerida, cascade)
+            modelBuilder.Entity<Room>()
+                .HasOne(r=> r.Conference)
+                .WithMany(c=>c.Rooms)
+                .HasForeignKey(r => r.ConferenceId)
+                .OnDelete(DeleteBehavior.Cascade);
             // N:M con payload: Talk (clave compuesta)
+            modelBuilder.Entity<Talk>()
+                .HasKey(t => new { t.SpeakerId, t.RoomId, t.StartTime });
             // (Opcional) Índice único: Room.Name dentro de una Conference
-
+            modelBuilder.Entity<Room>()
+                .HasIndex(r => new { r.ConferenceId, r.Name })
+                .IsUnique();
         }
     }
 }
