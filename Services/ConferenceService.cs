048ddbca57d0428cb09cf1beb5c910dbc99ad6f6
Juanma <juanma.soruco@gmail.com>
2025-12-05 16:31:28 -0400

    feat:add ConferenceService CreateConferenceAsync function

--- Services/ConferenceService.cs
+++ Services/ConferenceService.cs
@@ -6,11 +6,16 @@ namespace _3ecexamen.Services
 {
     public class ConferenceService : IConferenceService
     {
-        //TODO
+        private readonly IConferenceRepository _conferences;
+
+        public ConferenceService(IConferenceRepository conferences) => _conferences = conferences;
 
         public async Task<int> CreateConferenceAsync(CreateConferenceDto dto)
         {
-            //TODO
+            var entity = new Conference { Title = dto.Title, City = dto.City, StartDate = dto.StartDate, EndDate = dto.EndDate };
+            await _conferences.AddAsync(entity);
+            await _conferences.SaveChangesAsync();
+            return entity.Id;
         }
 
         public async Task<ConferenceAgendaDto?> GetAgendaAsync(int id)
