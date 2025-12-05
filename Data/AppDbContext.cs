461df8926907929cb780246d846d65cd31d8e74d
unknown <nicole.herbas@ucb.edu.bo>
2025-12-05 16:15:47 -0400

    feat: adding dbContext 1:N Conference -> Rooms

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -17,10 +17,35 @@ namespace _3ecexamen.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //TODO
+            base.OnModelCreating(modelBuilder);
 
             // 1:N Conference -> Rooms (FK requerida, cascade)
+            modelBuilder.Entity<Conference>(c =>
+            {
+                c.HasMany(c => c.Rooms)
+                .WithOne(room => room.Conference)
+                .HasForeignKey(room => room.ConferenceId)
+                .OnDelete(DeleteBehavior.Cascade);
+            });
+
             // N:M con payload: Talk (clave compuesta)
+            modelBuilder.Entity<Talk>()
+                .HasKey(t => new { t.SpeakerId, t.RoomId, t.StartTime });
+
+            modelBuilder.Entity<Talk>()
+                .HasOne(t => t.Speaker)
+                .WithMany(s => s.Talks)
+                .HasForeignKey(t => t.SpeakerId);
+
+            modelBuilder.Entity<Talk>()
+                .HasOne(t => t.Room)
+                .WithMany(r => r.Talks)
+                .HasForeignKey(t => t.RoomId);
+
             // (Opcional) Índice único: Room.Name dentro de una Conference
+            modelBuilder.Entity<Room>()
+                .HasIndex(r => r.Name)
+                .IsUnique();
 
         }
     }
