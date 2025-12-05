91512dfa3080d7db357309488004c86d6a0be929
unknown <nicole.herbas@ucb.edu.bo>
2025-12-05 16:26:33 -0400

    feat: adding CreateConferenceAsnc to ConferenceService

--- Services/ConferenceService.cs
+++ Services/ConferenceService.cs
@@ -1,16 +1,31 @@
 ﻿using _3ecexamen.DTOs;
 using _3ecexamen.Entities;
 using _3ecexamen.Repositories;
+using System.ComponentModel.DataAnnotations;
 
 namespace _3ecexamen.Services
 {
     public class ConferenceService : IConferenceService
     {
         //TODO
+        private readonly IConferenceRepository _confs;
+
+        public ConferenceService(IConferenceRepository conferences) => _confs = conferences;
 
         public async Task<int> CreateConferenceAsync(CreateConferenceDto dto)
         {
             //TODO
+            var entity = new Conference
+            {
+                Title = dto.Title,
+                City = dto.City,
+                StartDate = DateTime.SpecifyKind(dto.StartDate, DateTimeKind.Utc),
+                EndDate = DateTime.SpecifyKind(dto.EndDate, DateTimeKind.Utc),
+                Rooms = dto.Rooms.Select(r => new Room { Name = r.Name }).ToList()
+            };
+            await _confs.AddAsync(entity);
+            await _confs.SaveChangesAsync();
+            return entity.Id;
         }
 
         public async Task<ConferenceAgendaDto?> GetAgendaAsync(int id)
